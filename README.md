600cb777e87d8d09c9d12ffd010f4444409d47b1
Nariman Safiulin <woofilee@gmail.com>
2017-06-26 04:24:36 +0300

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+# The Bestory Magic-Powered API Server
+
+[![Updates](https://pyup.io/repos/github/thebestory/api/shield.svg)](https://pyup.io/repos/github/thebestory/api/)
+[![Python 3](https://pyup.io/repos/github/thebestory/api/python-3-shield.svg)](https://pyup.io/repos/github/thebestory/api/)
---
+++ setup.py
@@ -0,0 +1,47 @@
+"""
+The Bestory Project
+"""
+
+from distutils.core import setup
+
+
+def content(filename, splitlines=False):
+    c = open(filename, "r").read()
+    return c.splitlines() if splitlines else c
+
+
+long_description = content("README.md")
+
+install_requires = content("requirements.txt", splitlines=True)
+tests_requires = content("requirements-test.txt", splitlines=True)
+
+setup(
+    name="thebestory",
+    description="The Bestory Magic-Powered API Server",
+    version="2017.7.0",
+    url="thebestory.com",
+
+    author="The Bestory Team",
+    author_email="team@thebestory.com",
+
+    license="",
+    long_description=long_description,
+
+    packages=["tbs"],
+
+    install_requires=install_requires,
+    tests_require=install_requires + tests_requires,
+
+    classifiers=(
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Natural Language :: Russian",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3 :: Only",
+    ),
+)
